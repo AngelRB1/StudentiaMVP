a6f96e7225fc6a0db18910e06b6758c8a7cd4602
AngelRB1 <angel.lopez75@uabc.edu.mx>
2025-06-09 01:34:26 -0700

    DIR Prueba

--- settings.py
+++ settings.py
@@ -81,7 +81,7 @@ ROOT_URLCONF = 'studentiaTEDS.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],
+        'DIRS': [BASE_DIR / "studentiaTEDS" / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
