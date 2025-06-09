542489f4114f6d66f84c0f3759f64c0388672bd4
AngelRB1 <angel.lopez75@uabc.edu.mx>
2025-06-09 01:05:47 -0700

    basedatos postgresql

--- studentiaTEDS/settings.py
+++ studentiaTEDS/settings.py
@@ -100,10 +100,10 @@ WSGI_APPLICATION = 'studentiaTEDS.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default' : dj_database_url.config(
+        default='postgresql://postgres:postgres@localhost/postgres',
+        conn_max_age=600
+    )
 }
 
 
